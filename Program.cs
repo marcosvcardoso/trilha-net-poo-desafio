b304cf1d31ef4335050f81d45a644426c721c271
Marcos Vinicius Cardoso <marcosvcardoso@outlook.com>
2025-08-25 01:22:29 -0300

    desafio celular finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "987654321", modelo: "Modelo A", imei: "01011020101", memoria: 4);
+nokia.Ligar();
+nokia.InstalarAplicativo("zap");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "66666666", modelo: "b", imei: "55555555555", memoria: 8);
+iphone.Ligar();
+nokia.InstalarAplicativo("insta");
